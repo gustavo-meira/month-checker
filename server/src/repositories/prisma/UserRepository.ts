d859f8a1e98cb928ff0ccecb74bec3aeb3c3d763
Gustavo Wolfi <gmeira.antonio@gmail.com>
2022-06-12 18:32:08 -0300

    build<server>: Creating prisma user repository that implements user repository

---
+++ server/src/repositories/prisma/UserRepository.ts
@@ -0,0 +1,93 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+import { UserEntity } from '../../entities/UserEntity';
+import { NotFoundError } from '../../errors/NotFoundError';
+import { IUserRepository } from '../IUserRepository';
+
+export class UserRepository implements IUserRepository {
+  private prisma = new PrismaClient();
+
+  async create(user: UserEntity): Promise<void> {
+    await this.prisma.user.create({
+      data: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        password: user.password,
+      },
+    });
+  }
+
+  async getByEmail(email: string): Promise<UserEntity> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) throw new NotFoundError('User not found');
+
+    const userFounded = new UserEntity({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      password: {
+        encrypted: true,
+        value: user.password,
+      },
+    });
+
+    return userFounded;
+  }
+
+  async getByUsername(username: string): Promise<UserEntity> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) throw new NotFoundError('User not found');
+
+    const userFounded = new UserEntity({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      password: {
+        encrypted: true,
+        value: user.password,
+      },
+    });
+
+    return userFounded;
+  }
+
+  async getById(id: string): Promise<UserEntity> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) throw new NotFoundError('User not found');
+
+    const userFounded = new UserEntity({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      password: {
+        encrypted: true,
+        value: user.password,
+      },
+    });
+
+    return userFounded;
+  }
+
+  /* eslint class-methods-use-this: "off" */
+  async validatePassword(user: UserEntity, password: string): Promise<boolean> {
+    const isValid = await bcrypt.compare(password, user.password);
+
+    return isValid;
+  }
+}
