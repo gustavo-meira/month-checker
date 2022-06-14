c19fd09262ab6d9cf2c01771aca7e8fec483a27e
Gustavo Wolfi <gmeira.antonio@gmail.com>
2022-06-14 07:19:59 -0300

    test<server>: Create tests for user/email exist use case

---
+++ server/src/useCases/UserEmailExist/UserEmailExist.test.ts
@@ -0,0 +1,105 @@
+import sinon from 'sinon';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+import { app } from '../../app';
+import { UserRepository } from '../../repositories/prisma/UserRepository';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+
+describe('User/Email exist use case', () => {
+  beforeEach(() => {
+    sinon
+      .stub(UserRepository.prototype, 'getByEmail')
+      .resolves();
+
+    sinon
+      .stub(UserRepository.prototype, 'getByUsername')
+      .resolves();
+  });
+
+  afterEach(() => {
+    (UserRepository.prototype.getByEmail as sinon.SinonStub).restore();
+    (UserRepository.prototype.getByUsername as sinon.SinonStub).restore();
+  });
+
+  it('should return ok status when the username does not exist', async () => {
+    const response = await chai
+      .request(app)
+      .post('/user/check?username=test')
+      .send();
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal({});
+  });
+
+  it('should return ok status when the email does not exist', async () => {
+    const response = await chai
+      .request(app)
+      .post('/user/check?email=test@test.com')
+      .send();
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal({});
+  });
+
+  it('should return ok status when the username and email does not exist', async () => {
+    const response = await chai
+      .request(app)
+      .post('/user/check?username=test&email=test@test.com')
+      .send();
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal({});
+  });
+
+  it('should return a bad request error when the username and email is not provided', async () => {
+    const response = await chai
+      .request(app)
+      .post('/user/check')
+      .send();
+
+    expect(response.status).to.equal(400);
+    expect(response.body.message).to.equal('"username" or "email" is required');
+  });
+
+  it('should return a conflict error when the username exists', async () => {
+    (UserRepository.prototype.getByUsername as sinon.SinonStub).throwsException();
+
+    const response = await chai
+      .request(app)
+      .post('/user/check?username=test')
+      .send();
+
+    expect(response.status).to.equal(409);
+    expect(response.body).to.deep.equal({});
+  });
+
+  it('should return a conflict error when the email exists', async () => {
+    (UserRepository.prototype.getByEmail as sinon.SinonStub).throwsException();
+
+    const response = await chai
+      .request(app)
+      .post('/user/check?email=test@test.com')
+      .send();
+
+    expect(response.status).to.equal(409);
+    expect(response.body).to.deep.equal({});
+  });
+
+  it('should return a conflict error when the username and email exists', async () => {
+    (UserRepository.prototype.getByUsername as sinon.SinonStub).throwsException();
+    (UserRepository.prototype.getByEmail as sinon.SinonStub).throwsException();
+
+    const response = await chai
+      .request(app)
+      .post('/user/check?username=test&email=test@test.com')
+      .send();
+
+    expect(response.status).to.equal(409);
+    expect(response.body).to.deep.equal({});
+  });
+});
