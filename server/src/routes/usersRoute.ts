2255b0dbd698ae1b14d4801daf8eca35e1865a0e
Gustavo Wolfi <gmeira.antonio@gmail.com>
2022-06-14 07:25:58 -0300

    feat<server>: creating route /user/check that uses the user/email exist use case

--- server/src/routes/usersRoute.ts
+++ server/src/routes/usersRoute.ts
@@ -3,11 +3,16 @@ import {
 } from 'express';
 import { validateCreateUser } from '../middlewares/validateCreateUser';
 import { createUserController } from '../useCases/CreateUser';
+import { userEmailExistController } from '../useCases/UserEmailExist';
 
 const usersRouter = Router();
 
 usersRouter.post('/', validateCreateUser, (req: Request, res: Response, next: NextFunction) => {
   createUserController.handle(req, res, next);
+});
+
+usersRouter.post('/check', (req: Request, res: Response, next: NextFunction) => {
+  userEmailExistController.handle(req, res, next);
 });
 
 export { usersRouter };
